ca5adfd5fa64d0c27d2291659c1af4d6d879c77b
delma12 <delma.jldnmcr2022@gmail.com>
2025-03-17 15:31:31 +0000

    changes to static

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from models import Apprentice, Review, User
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 load_dotenv()
 
